e02e8e3070e61752f7f31b36fad68660b1be1e7a
Denilson <denilson.gomes@elogroup.com.br>
2024-10-28 18:07:56 -0300

    implemented get/post/put method

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,46 @@
+import re
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
-
+def test_root_deve_retornar_html_com_frase_inicial(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Olá mundo!'}
+    assert response.headers['content-type'] == 'text/html; charset=utf-8'
+    assert (
+        re.search(r'<h1 style="[^"]*">Cadastro de pessoas</h1>', response.text)
+        is not None
+    )
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'Jon Snow',
+            'email': 'jon.snow@got.com',
+            'password': 'kingofthenorth',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'Jon Snow',
+        'email': 'jon.snow@got.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'Jon Snow',
+                'email': 'jon.snow@got.com',
+                'id': 1
+            }
+        ]
+    }
